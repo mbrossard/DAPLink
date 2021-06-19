5284ae821baff639a38fda813eaa2acef3d0fd65
Mathias Brossard <mathias@brossard.org>
2021-06-19 01:32:26 -0500

    stm32f4x1ce_hic: configure LED

--- source/hic_hal/stm32/stm32f4x1ce/gpio.c
+++ source/hic_hal/stm32/stm32f4x1ce/gpio.c
@@ -23,18 +23,53 @@
 
 void gpio_init(void)
 {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+
+    HAL_GPIO_WritePin(RUNNING_LED_PORT, RUNNING_LED_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(CONNECTED_LED_PORT, CONNECTED_LED_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(PIN_HID_LED_PORT, PIN_HID_LED, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(PIN_CDC_LED_PORT, PIN_CDC_LED, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(PIN_MSC_LED_PORT, PIN_MSC_LED, GPIO_PIN_RESET);
+
+    /* Configure GPIO pin */
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    GPIO_InitStruct.Pin = RUNNING_LED_PIN;
+    HAL_GPIO_Init(RUNNING_LED_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = CONNECTED_LED_PIN;
+    HAL_GPIO_Init(CONNECTED_LED_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = PIN_HID_LED;
+    HAL_GPIO_Init(PIN_HID_LED_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = PIN_CDC_LED;
+    HAL_GPIO_Init(PIN_CDC_LED_PORT, &GPIO_InitStruct);
+    GPIO_InitStruct.Pin = PIN_MSC_LED;
+    HAL_GPIO_Init(PIN_MSC_LED_PORT, &GPIO_InitStruct);
 }
 
 void gpio_set_hid_led(gpio_led_state_t state)
 {
+    // LED is active low
+    HAL_GPIO_WritePin(PIN_HID_LED_PORT, PIN_HID_LED, state ? GPIO_PIN_RESET : GPIO_PIN_SET);
 }
 
 void gpio_set_cdc_led(gpio_led_state_t state)
 {
+    // LED is active low
+    HAL_GPIO_WritePin(PIN_CDC_LED_PORT, PIN_CDC_LED, state ? GPIO_PIN_RESET : GPIO_PIN_SET);
 }
 
 void gpio_set_msc_led(gpio_led_state_t state)
 {
+    // LED is active low
+    HAL_GPIO_WritePin(PIN_MSC_LED_PORT, PIN_MSC_LED, state ? GPIO_PIN_RESET : GPIO_PIN_SET);
 }
 
 uint8_t gpio_get_reset_btn_no_fwrd(void)
