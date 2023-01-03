24a3cfdb77508da82da3882d18d8e07753ea3e17
Mathias Brossard <mathias@brossard.org>
2023-01-02 21:34:44 -0600

    stm32f723ie_hic: add support for 32F723EDISCOVERY and hs-probe

--- source/hic_hal/stm32/stm32f723ie/IO_Config.h
+++ source/hic_hal/stm32/stm32f723ie/IO_Config.h
@@ -33,6 +33,8 @@
 // This GPIO configuration is only valid for the STM32F723IE HIC
 COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F723IE);
 
+#if defined(STLINKV3)
+
 // Pin mapping from STLINK-V3MINI_BSP.h in
 // https://github.com/RadioOperator/CMSIS-DAP_for_STLINK-V3MINI
 // Debug Port I/O Pins for STM32F723, matched with STLINK-V3MINI board
@@ -94,5 +96,131 @@ COMPILER_ASSERT(DAPLINK_HIC_ID == DAPLINK_HIC_ID_STM32F723IE);
 // Target Running LED (Output)  PA10 - Low,  GREEN
 #define LED_RUNNING_PORT        GPIOA
 #define LED_RUNNING_BIT         GPIO_PIN_10
+
+#elif defined(HS_PROBE)
+
+// SWCLK/TCK (Input)            PB3 (SPI1_CLK)
+#define PIN_SWCLK_TCK_I_PORT    GPIOB
+#define PIN_SWCLK_TCK_I         GPIO_PIN_3
+#define PIN_SWCLK_TCK_I_Bit     3
+
+// SWCLK/TCK (Output)           PI1 (SPI2_CLK)
+#define PIN_SWCLK_TCK_O_PORT    GPIOI
+#define PIN_SWCLK_TCK_O         GPIO_PIN_1
+#define PIN_SWCLK_TCK_O_Bit     1
+
+// SWDIO/TMS (Input)            PB4 (SPI1_MISO)
+#define PIN_SWDIO_TMS_I_PORT    GPIOB
+#define PIN_SWDIO_TMS_I         GPIO_PIN_4
+#define PIN_SWDIO_TMS_I_Bit     4
+
+// SWDIO/TMS (Output)           PB5 (SPI1_MOSI)
+#define PIN_SWDIO_TMS_O_PORT    GPIOB
+#define PIN_SWDIO_TMS_O         GPIO_PIN_5
+#define PIN_SWDIO_TMS_O_Bit     5
+
+// TDI (Input)                  Not available
+// #define PIN_TDI_I_PORT
+// #define PIN_TDI_I
+// #define PIN_TDI_I_Bit
+
+// TDI (Output)                 PI3 (SPI2_MOSI)
+#define PIN_TDI_O_PORT          GPIOI
+#define PIN_TDI_O               GPIO_PIN_3
+#define PIN_TDI_O_Bit           3
+
+// TDO (Input)                  PI2 (SPI2_MISO)
+#define PIN_TDO_PORT            GPIOI
+#define PIN_TDO                 GPIO_PIN_2
+#define PIN_TDO_Bit             2
+
+// nTRST Pin                    Not available
+// #define PIN_nTRST_PORT
+// #define PIN_nTRST
+// #define PIN_nTRST_Bit
+
+// nRESET Pin                   PA6
+#define PIN_nRESET_PORT         GPIOA
+#define PIN_nRESET              GPIO_PIN_6
+#define PIN_nRESET_Bit          6
+
+// nRESET Output Enable         Not available
+// #define PIN_nRESET_OE_PORT
+// #define PIN_nRESET_OE
+// #define PIN_nRESET_OE_Bit
+
+// DAP LED (Output)            PC10 (LED1)
+#define PIN_DAP_LED_PORT       GPIOC
+#define PIN_DAP_LED            GPIO_PIN_10
+#define PIN_DAP_LED_Bit        10
+
+// CDC LED (Output)            PB7 (LED2)
+#define PIN_CDC_LED_PORT       GPIOB
+#define PIN_CDC_LED            GPIO_PIN_7
+#define PIN_CDC_LED_Bit        7
+
+// MSC CDC (Output)            PE0 (LED3)
+#define PIN_MSC_LED_PORT       GPIOE
+#define PIN_MSC_LED            GPIO_PIN_0
+#define PIN_MSC_LED_Bit        0
+
+#elif defined(DISCOVERY)
+
+// SWCLK/TCK (Input)            PE3 (ARD D7)
+#define PIN_SWCLK_TCK_I_PORT    GPIOE
+#define PIN_SWCLK_TCK_I         GPIO_PIN_3
+#define PIN_SWCLK_TCK_I_Bit     3
+
+// SWCLK/TCK (Output)           PE6 (ARD D6)
+#define PIN_SWCLK_TCK_O_PORT    GPIOE
+#define PIN_SWCLK_TCK_O         GPIO_PIN_6
+#define PIN_SWCLK_TCK_O_Bit     6
+
+// SWDIO/TMS (Input)            PB4 (ARD D12, MISO1)
+#define PIN_SWDIO_TMS_I_PORT    GPIOB
+#define PIN_SWDIO_TMS_I         GPIO_PIN_4
+#define PIN_SWDIO_TMS_I_Bit     4
+
+// SWDIO/TMS (Output)           PB5 (ARD D11, MOSI1)
+#define PIN_SWDIO_TMS_O_PORT    GPIOB
+#define PIN_SWDIO_TMS_O         GPIO_PIN_5
+#define PIN_SWDIO_TMS_O_Bit     5
+
+// TDI (Input)                  PH3 (ARD D4)
+#define PIN_TDI_I_PORT          GPIOH
+#define PIN_TDI_I               GPIO_PIN_4
+#define PIN_TDI_I_Bit           4
+
+// TDI (Output)                 PE5 (ARD D3)
+#define PIN_TDI_O_PORT          GPIOE
+#define PIN_TDI_O               GPIO_PIN_5
+#define PIN_TDI_O_Bit           5
+
+// TDO (Input)                  PA1 (ARD D10, NSS1, UART4_RX AF8)
+#define PIN_TDO_PORT            GPIOA
+#define PIN_TDO                 GPIO_PIN_1
+#define PIN_TDO_Bit             1
+
+// nRESET Pin                   PA6 (ARD A0)
+#define PIN_nRESET_PORT         GPIOA
+#define PIN_nRESET              GPIO_PIN_6
+#define PIN_nRESET_Bit          6
+
+// DAP LED (Output blue)       PA5 (ARDUINO)
+#define PIN_DAP_LED_PORT       GPIOA
+#define PIN_DAP_LED            GPIO_PIN_5
+#define PIN_DAP_LED_Bit        5
+
+// CDC LED (Output red)        PA7 (USER1)
+#define PIN_CDC_LED_PORT       GPIOA
+#define PIN_CDC_LED            GPIO_PIN_7
+#define PIN_CDC_LED_Bit        7
+
+// MSC CDC (Output green)      PB1 (USER2)
+#define PIN_MSC_LED_PORT       GPIOB
+#define PIN_MSC_LED            GPIO_PIN_1
+#define PIN_MSC_LED_Bit        1
+
+#endif
 
 #endif
--- source/hic_hal/stm32/stm32f723ie/sdk.c
+++ source/hic_hal/stm32/stm32f723ie/sdk.c
@@ -40,6 +40,80 @@ void sdk_init()
     SystemCoreClockUpdate();
 }
 
+#if defined(HS_PROBE)
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 12000000
+  *            PLL_M                          = 6
+  *            PLL_N                          = 216
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 9
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 7
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config()
+{
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 6;
+    RCC_OscInitStruct.PLL.PLLN = 216;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        /* Initialization Error */
+        util_assert(0);
+    }
+
+    /* Activate the OverDrive to reach the 216 Mhz Frequency */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+        /* Initialization Error */
+        util_assert(0);
+    }
+
+    /* Select the PLL as system clock source and configure the HCLK, PCLK1 and
+       PCLK2 clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+                                   RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+        /* Initialization Error */
+        util_assert(0);
+    }
+
+    PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_CLK48;
+    PeriphClkInitStruct.Clk48ClockSelection = RCC_CLK48SOURCE_PLL;
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK) {
+        /* Initialization Error */
+        util_assert(0);
+    }
+}
+
+#else
 /**
   * @brief  System Clock Configuration
   *         The system Clock is configured as follow :
@@ -84,8 +158,7 @@ void SystemClock_Config()
     }
 
     /* Activate the OverDrive to reach the 216 Mhz Frequency */
-    if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-    {
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
         /* Initialization Error */
         util_assert(0);
     }
@@ -103,3 +176,4 @@ void SystemClock_Config()
         util_assert(0);
     }
 }
+#endif
