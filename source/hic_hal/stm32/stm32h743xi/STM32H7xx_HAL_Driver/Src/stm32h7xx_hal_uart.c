960da41b7c034cc58a00c72c3fc44b41aaf402fd
Mathias Brossard <mathias@brossard.org>
2023-02-10 21:43:35 -0600

    stm32h743xi_hic: name collision in STM32H7xx HAL

--- source/hic_hal/stm32/stm32h743xi/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c
+++ source/hic_hal/stm32/stm32h743xi/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c
@@ -1144,7 +1144,7 @@ HAL_StatusTypeDef HAL_UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, u
     huart->TxXferCount = Size;
 
     /* In case of 9bits/No Parity transfer, pData needs to be handled as a uint16_t pointer */
-    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
     {
       pdata8bits  = NULL;
       pdata16bits = (uint16_t *) pData;
@@ -1239,7 +1239,7 @@ HAL_StatusTypeDef HAL_UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, ui
     uhMask = huart->Mask;
 
     /* In case of 9bits/No Parity transfer, pRxData needs to be handled as a uint16_t pointer */
-    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
     {
       pdata8bits  = NULL;
       pdata16bits = (uint16_t *) pData;
@@ -1317,7 +1317,7 @@ HAL_StatusTypeDef HAL_UART_Transmit_IT(UART_HandleTypeDef *huart, uint8_t *pData
     if (huart->FifoMode == UART_FIFOMODE_ENABLE)
     {
       /* Set the Tx ISR function pointer according to the data word length */
-      if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+      if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
       {
         huart->TxISR = UART_TxISR_16BIT_FIFOEN;
       }
@@ -1334,7 +1334,7 @@ HAL_StatusTypeDef HAL_UART_Transmit_IT(UART_HandleTypeDef *huart, uint8_t *pData
     else
     {
       /* Set the Tx ISR function pointer according to the data word length */
-      if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+      if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
       {
         huart->TxISR = UART_TxISR_16BIT;
       }
@@ -3480,7 +3480,7 @@ HAL_StatusTypeDef UART_Start_Receive_IT(UART_HandleTypeDef *huart, uint8_t *pDat
   if ((huart->FifoMode == UART_FIFOMODE_ENABLE) && (Size >= huart->NbRxDataToProcess))
   {
     /* Set the Rx ISR function pointer according to the data word length */
-    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
     {
       huart->RxISR = UART_RxISR_16BIT_FIFOEN;
     }
@@ -3498,7 +3498,7 @@ HAL_StatusTypeDef UART_Start_Receive_IT(UART_HandleTypeDef *huart, uint8_t *pDat
   else
   {
     /* Set the Rx ISR function pointer according to the data word length */
-    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == UART_PARITY_NONE))
+    if ((huart->Init.WordLength == UART_WORDLENGTH_9B) && (huart->Init.Parity == HAL_UART_PARITY_NONE))
     {
       huart->RxISR = UART_RxISR_16BIT;
     }
